13543826fefc6718ebf6f7619781a66e6391f5b9
Ashley Chen <ashley@ativegroup.com>
2017-10-04 10:53:05 -0700

    Handling HTTP requests with Node.js

--- app/app.js
+++ app/app.js
@@ -2,9 +2,17 @@
 // create a variable(object) called http to store the http module(library)
 var http = require('http');
 
-// create
+// createServer with a anonymous call back function
 var myServer = http.createServer(function(request, response){
-  response.writeHead(200, {"content-Type": "text/plain"});
 
-  response.write();
+  // set the head with 200 status code and the content type
+  response.writeHead(200, {"content-Type": "text/html"});
+
+  response.write("<h1>Hey, this is NodeJS/Express Server!!</h1>");
+  response.end();
 });
+
+// listen to a specific port
+myServer.listen(3002);
+
+console.log('Go to http://localhost:3002 on your browser');
